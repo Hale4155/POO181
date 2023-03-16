1e7b9918cd546749c98644fd5b2cd3376cb38ecf
Hale4155 <121038640@upq.edu.mx>
2023-03-15 18:57:07 -0600

    commit 18:50

--- Examen2dop/examen.py
+++ Examen2dop/examen.py
@@ -11,7 +11,9 @@ class GeneradorMatriculas:
         self.apellidopaterno_var = tk.StringVar()
         self.apellidomaterno_var = tk.StringVar()
         self.fecha_nacimiento_var = tk.StringVar()
+        self.fecha_actual_var = tk.StringVar()
         self.carrera_var = tk.StringVar()
+        self.digitosaleatorios_var= tk.StringVar()
         
         tk.Label(self.master, text="Nombre:").grid(row=0, column=0)
         tk.Entry(self.master, textvariable=self.nombre_var).grid(row=0, column=1)
@@ -28,8 +30,14 @@ class GeneradorMatriculas:
         tk.Label(self.master, text="Carrera:").grid(row=4, column=0)
         tk.Entry(self.master, textvariable=self.carrera_var).grid(row=4, column=1)
         
+        tk.Label(self.master, text="fecha actual:").grid(row=5, column=0)
+        tk.Entry(self.master, textvariable=self.fecha_actual_var).grid(row=5, column=1)
+        
+        tk.Label(self.master, text="digitos aleatorios:").grid(row=6, column=0)
+        tk.Entry(self.master, textvariable=self.digitosaleatorios_var).grid(row=6, column=1)
+        
         # Botón para generar la matrícula
-        tk.Button(self.master, text="Generar Matrícula", command=self.generar_matricula).grid(row=4, column=0, columnspan=2, pady=10)
+        tk.Button(self.master, text="Generar Matrícula", command=self.generar_matricula).grid(row=7, column=0, columnspan=2, pady=10)
         
         self.matricula_var = tk.StringVar()
         self.matricula_var.set("")
@@ -39,17 +47,39 @@ class GeneradorMatriculas:
         apellidopaterno = self.apellidopaterno_var.get()
         apellidomaterno = self.apellidomaterno_var.get()
         fecha_nacimiento = self.fecha_nacimiento_var.get()
+        fechaactual= self.fecha_actual_var.get()
         carrera = self.carrera_var.get()
+        digitos = self.digitosaleatorios_var.get()
         
-        # Extraer la letra del nombre, apellido y carrera
+        # Extraer la letra del nombre, apellido y carrera y los digitos
         letra_nombre = nombre[0]
-        letra_apellido = apellidopaterno[0]
-        letra_apellido= apellidomaterno[0]
-        letra_carrera = carrera[0]
+        letra_apellidopaterno = apellidopaterno[-2:]
+        letra_apellidomaterno= apellidomaterno[-2:]
+        letra_carrera = carrera[-3:]
         
         # Extraer los dos dígitos de la fecha de nacimiento
-        dia, mes, año = fecha_nacimiento.split("/")
-        digitos_fecha = año[-2:]
+        fecha = fecha_nacimiento
+        digitos_fecha1 = fecha[-2:]
+        
+        fecha = fechaactual
+        digitos_fecha2 = fecha [-2:]
+        
+        dos= digitos
+        digitos_2 = dos [-2:]
+        # Concatenar las partes para formar la matrícula
+        matricula = letra_nombre + letra_apellidopaterno + letra_apellidomaterno + letra_carrera + digitos_fecha1 + digitos_fecha2 + digitos_2
+        
+        # Asignar la matrícula al widget de salida
+        self.matricula_var.set(matricula)
+        
+        # Mostrar la matrícula en un messagebox
+        messagebox.showinfo("Matrícula generada", f"La matrícula generada es: {matricula}")
+        
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GeneradorMatriculas(root)
+    root.mainloop()
+        
         
         
         
